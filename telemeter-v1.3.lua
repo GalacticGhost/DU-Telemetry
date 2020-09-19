a3961c97fffd5c10bf0ca08b3d692df29e04f052
Brian <hackersafehouse@gmail.com>
2020-09-18 20:58:17 -0500

    refactored code

--- telemeter-v1.3.lua
+++ telemeter-v1.3.lua
@@ -1,6 +1,7 @@
---[[Description: Makes your telementer functional.
+--[[Description: Makes your telemeter functional.
 
-    Directions: 1. Place telemeter underneath the ship.
+    Directions: Place this code in either a chair or programming board, either way will work.
+                1. Place telemeter underneath the ship.
                 2. Link a landing gear and either name slot to gear_1 or ajdust gear_1 in the code
                    to match whatever you name it.
                 3. Link to telemeter and name slot telemeter or adjust code to match whatever you name it.]]
@@ -8,30 +9,23 @@
 --[[TODO: install a second telemeter and make it show information when about to collide with an object while traveling in a longitudinal direction. Give it a flashing red background when proximity to obstacle is too close when landing gear is raised.]]
 
 --Place in system>start()
+local altitude = string.format(%.2f, tostring(telemeter.getDistance()))
 
---Makes information visible.
-dist = telemeter.getDistance()
+--Makes information visible if telemetry is available (telemeter has a max distance of 20m)
 
-if dist <= 20 then
+if altitude <= 20 then
     system.showScreen(1)
 else
     system.showScreen(0)	
 end
 
-local function round(val, decimal)
-  if (decimal) then
-    return math.floor( (val * 10^decimal) + 0.5) / (10^decimal)
-  else
-    return math.floor(val+0.5)
-  end
-end
-
 function updateDisplay()
-    local roundedDist = round(dist, 2)
+    local offsetDistance = 0
+    
     if gear_1.getState() == 1 then
-    	 offsetDist = roundedDist - 2.22
+    	 offsetDistance = altitude - 2.22
     else
-          offsetDist = roundedDist - 0.77
+          offsetDist = altitude - 0.77
     end
     content =
     		[[<head>
